7ca3e8f93468702aa87a38123e2f612d14673589
Lakshya <lakshyasharmaktl@gmail.com>
2026-06-23 08:25:07 +0530

    a

--- src/global_url.js
+++ src/global_url.js
@@ -1,3 +1,3 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:1234';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://loopix-backend-4.onrender.com';
 
 export default BASE_URL;
